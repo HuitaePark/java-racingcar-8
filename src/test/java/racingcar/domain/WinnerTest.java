63ac291421030e6dd65c5f42c33e27a14e55b2ea
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-24 03:16:42 +0900

    test(Winner): Winner 테스트 추가

---
+++ src/test/java/racingcar/domain/WinnerTest.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinnerTest {
+    @Test
+    @DisplayName("우승자 목록을 받아서 쉼표로 구분해 출력한다.")
+    void many_winning_cars() {
+        Winners winners = new Winners(List.of("김씨", "박씨", "심씨"));
+
+        assertThat(winners.toString()).isEqualTo("김씨,박씨,심씨");
+    }
+}
