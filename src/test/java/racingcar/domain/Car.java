a524f704a49f07a23b28b6360678beeb13ddfd6c
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-23 13:34:54 +0900

    feat(Car): 자동차의 이름이 5글자 이내인지 검증하는 기능 구현

--- src/test/java/racingcar/domain/Car.java
+++ src/test/java/racingcar/domain/Car.java
@@ -2,9 +2,13 @@ package racingcar.domain;
 
 public class Car {
 
+    private final String name;
     private Position position;
 
-    public Car() {
+
+    public Car(String name) {
+        verifyCarName(name);
+        this.name = name;
         this.position = new Position(0);
     }
 
@@ -16,5 +20,11 @@ public class Car {
 
     public int distance() {
         return this.position.value();
+    }
+
+    private void verifyCarName(String name){
+        if(name.length()>5){
+            throw new IllegalArgumentException("자동차의 이름은 5자를 넘을수 없습니다.");
+        }
     }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,7 @@ public class CarTest {
     @Test
     @DisplayName("4 이상의 값이 나올경우 한칸 진행한다.")
     void car_move_4_amount(){
-        Car car = new Car();
+        Car car = new Car("");
         car.move(4);
         assertThat(car.distance()).isEqualTo(1);
     }
@@ -18,8 +19,17 @@ public class CarTest {
     @Test
     @DisplayName("4 미만의 값이 나올경우 진행하지 않는다.")
     void car_dont_move_under_4(){
-        Car car = new Car();
+        Car car = new Car("");
         car.move(3);
         assertThat(car.distance()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("자동차의 이름이 5자가 넘으면 에러가 발생한다.")
+    void car_name_under_5(){
+        assertThatThrownBy(() -> {
+            Car car = new Car("overName");
+        })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
