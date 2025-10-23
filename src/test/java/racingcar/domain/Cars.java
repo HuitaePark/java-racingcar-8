c3f500b6ce97cc1849ae7a1600c8efa4f3dc82cd
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-23 14:42:02 +0900

    feat(Cars): 입력받은 이름이 null이나 빈문자열이 있는지 검증하는 기능 구현

--- src/test/java/racingcar/domain/Cars.java
+++ src/test/java/racingcar/domain/Cars.java
@@ -8,6 +8,8 @@ public class Cars {
 
     public Cars(List<String> inputList) {
         verifyDuplicationName(inputList);
+        verifyEmptyName(inputList);
+        verifyNull(inputList);
         this.carList = convertFrom(inputList);
     }
 
@@ -16,9 +18,7 @@ public class Cars {
     }
 
     private List<Car> convertFrom(List<String> inputList) {
-        return inputList.stream()
-                .map(Car::new)
-                .toList();
+        return inputList.stream().map(Car::new).toList();
     }
 
     private void verifyDuplicationName(List<String> inputList) {
@@ -28,8 +28,18 @@ public class Cars {
     }
 
     private int getDistinctSize(List<String> inputList) {
-        return (int) inputList.stream()
-                .distinct()
-                .count();
+        return (int) inputList.stream().distinct().count();
+    }
+
+    private void verifyEmptyName(List<String> inputList) {
+        if (inputList.contains("")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void verifyNull(List<String> inputList) {
+        if (inputList.contains(null)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,7 @@ public class CarsTest {
     @Test
     @DisplayName("입력받은 이름들로 자동차 객체들을 생성한다.")
     void make_car_list() {
-        List<String> inputList = List.of("김씨", "이씨", "박씨");
+        List<String> inputList = Arrays.asList("김씨", "이씨", "박씨");
 
         Cars cars = new Cars(inputList);
 
@@ -22,9 +23,27 @@ public class CarsTest {
     }
 
     @Test
-    @DisplayName("입력받은 이름이 중복되어 있는지 검증한다.")
+    @DisplayName("입력받은 이름이 중복되어 있으면 에러가 발생한다.")
     void verify_car_name_duplication() {
         List<String> inputList = List.of("김씨", "김씨", "심씨");
+
+        assertThatThrownBy(() -> new Cars(inputList))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력받은 이름에 빈 문자열이 포함되면 에러가 발생한다.")
+    void verify_car_name_empty() {
+        List<String> inputList = Arrays.asList("", "김씨", "심씨");
+
+        assertThatThrownBy(() -> new Cars(inputList))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력받은 이름에 null이 포함되면 에러가 발생한다.")
+    void verify_car_name_null() {
+        List<String> inputList = Arrays.asList(null, "김씨", "심씨");
 
         assertThatThrownBy(() -> new Cars(inputList))
                 .isInstanceOf(IllegalArgumentException.class);
