d5f915853beca27337b5f7329b49c24af9382673
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-23 13:39:58 +0900

    refactor(CarTest): 자동차의 이름 검증 테스트 중괄호 제거 리팩터링

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -10,7 +10,7 @@ public class CarTest {
 
     @Test
     @DisplayName("4 이상의 값이 나올경우 한칸 진행한다.")
-    void car_move_4_amount(){
+    void car_move_4_amount() {
         Car car = new Car("");
         car.move(4);
         assertThat(car.distance()).isEqualTo(1);
@@ -18,7 +18,7 @@ public class CarTest {
 
     @Test
     @DisplayName("4 미만의 값이 나올경우 진행하지 않는다.")
-    void car_dont_move_under_4(){
+    void car_dont_move_under_4() {
         Car car = new Car("");
         car.move(3);
         assertThat(car.distance()).isEqualTo(0);
@@ -26,10 +26,9 @@ public class CarTest {
 
     @Test
     @DisplayName("자동차의 이름이 5자가 넘으면 에러가 발생한다.")
-    void car_name_under_5(){
-        assertThatThrownBy(() -> {
-            Car car = new Car("overName");
-        })
-                .isInstanceOf(IllegalArgumentException.class);
+    void car_name_under_5() {
+        assertThatThrownBy(() -> new Car("overName")
+        ).isInstanceOf(IllegalArgumentException.class);
     }
+
 }
