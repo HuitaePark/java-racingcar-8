a2c102d92d490179cbea37f9a22920442e7b6706
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-25 01:10:21 +0900

    test(InputValidatorTest): 성공 테스트케이스 추가

--- src/test/java/racingcar/presentation/InputValidatorTest.java
+++ src/test/java/racingcar/presentation/InputValidatorTest.java
@@ -1,5 +1,6 @@
 package racingcar.presentation;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +19,18 @@ public class InputValidatorTest {
     void input_validate_separation_by_comma(String input) {
         assertThatThrownBy(() -> InputValidator.validateName(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("정상적인 이름이 들어올경우 에러가 발생하지않는다.")
+    @ParameterizedTest
+    @CsvSource({
+            "pobi",
+            "박희태,1234,abc-_-def",
+            "'이름1, 이름2, 이름3'"
+    })
+    void input_separation_success_by_comma(String input) {
+        assertThatCode(() -> InputValidator.validateName(input))
+                .doesNotThrowAnyException();
     }
 
 }
