ce9e9fb978a1bcf66d6d0c8895adbe20d83aaea4
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-26 17:55:34 +0900

    test(IntegrationTest): 통합테스트 추가

---
+++ src/test/java/racingcar/IntegrationTest.java
@@ -0,0 +1,69 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class IntegrationTest extends NsTest {
+    private final int MOVING_FORWARD = 4;
+
+    @Test
+    @DisplayName("이름에 콤마가 들어올 경우 에러 발생")
+    void name_is_comma_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",,,,", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("횟수가 너무 많을 경우 에러 발생")
+    void toManny_number_is_error() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("막스,르끌레르,노리스,오스카", "9999999999999999999999999"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("단일 자동차일때 정상적으로 작동한다.")
+    void worksNormallyWithSingleCar() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi", "1");
+                    assertThat(output())
+                            .contains("pobi : -")
+                            .contains("최종 우승자 : pobi");
+                },
+                MOVING_FORWARD
+        );
+    }
+
+    @DisplayName("10대의 자동차가 정상적으로 경주를 진행한다")
+    @Test
+    void worksNormallyWithTenCars() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("car1,car2,car3,car4,car5,car6,car7,car8,car9,car10", "1");
+                    assertThat(output())
+                            .contains(
+                                    "car1 : -", "car2 : -", "car3 : -", "car4 : -", "car5 : -",
+                                    "car6 : -", "car7 : -", "car8 : -", "car9 : -", "car10 : -",
+                                    "최종 우승자 :"
+                            );
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
