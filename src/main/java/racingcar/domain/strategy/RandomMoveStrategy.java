c562c6ba4488672158c0debb15784cfe51022722
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-27 19:15:42 +0900

    refactor(RandomMoveStrategy): 매직넘버 추출

--- src/main/java/racingcar/domain/strategy/RandomMoveStrategy.java
+++ src/main/java/racingcar/domain/strategy/RandomMoveStrategy.java
@@ -3,8 +3,10 @@ package racingcar.domain.strategy;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveStrategy implements MoveStrategy {
+    private static final int MOVE_THRESHOLD = 4;
+
     @Override
     public boolean isMoveable() {
-        return Randoms.pickNumberInRange(0, 9) >= 4;
+        return Randoms.pickNumberInRange(0, 9) >= MOVE_THRESHOLD;
     }
 }
